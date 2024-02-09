3525c886b43d8e01a2bc791e5adceaafcc115c37
TheJerryCann <122393082+TheJerryCann@users.noreply.github.com>
2024-02-09 13:53:31 -0500

    w5

--- app/models/TaskModel.php
+++ app/models/TaskModel.php
@@ -3,7 +3,62 @@
 // global $db originate from /db.php
 include(__DIR__ . '/../database/db.php');
 
+function getTasks () {
+    global $db;
+    $results = [];
+    $stmt = $db->prepare("SELECT task_id, list_id, title, description, due_date, is_favorite, created_at, updated_at From tasks ORDER BY due_date")
 
+    if ($stmt->execute() && $stmt->rowcount() > 0 ) {
+        $results = $stmt->fetchALL(PDO::FETCH_ASSOC);
+    }
+    return($results);
+}
+
+function addTask ($t, $d, $dd, $if){
+    global $db;
+    $stmt = $db->prepare("INSERT INTO tasks SET title = :title, description = :description, due_date = :due_date, is_favorite = :is_favorite");
+    $binds = array(
+        ":title" => $t,
+        ":description" => $d,
+        ":due_date" => $dd,
+        ":is_favorite" => $isf
+    );
+    if ($stmt->execute($binds) && $stmt->rowCount() > 0) {
+        $results = 'Data Added';
+    }
+    return ($results);
+}
+$tasks = getTasks();
+
+function deleteTask ($task_id){
+    global $db;
+    $results = [];
+    $sql = "DELETE FROM tasks WHERE task_id = ;task_id";
+    $stmt = $db->prepare("DELETE FROM tasks WHERE task_id = :task_id")
+    $binds = array(
+        ":task_id" => $task_id
+    );
+    if($stmt->execute($binds) && $stmt->rowCount() > 0) {
+        $results = 'Data Deleted';
+    }
+    return ($results);
+}
+
+function updateTask ($task_id, $title, $description, $due_date, $is_favorite, $created_at, $updated_at){
+    $results = [];
+    $sql = "UPDATE tasks SET title = :t, description = :d, due_date = :dd, is_favorite = :isf WHERE task_id = task_id";
+    $stmt = $db->prepare("UPDATE tasks SET title = :t, description = :d, due_date = :dd, is_favorite = :isf WHERE task_id = ;task_id");
+    $binds = array(
+        ":title" => $t,
+        ":description" => $d,
+        ":due_date" => $dd,
+        ":is_favorite" => $isf
+    );
+    if($stmt->execute($binds) && $stmt->rowCount() > 0) {
+        $results = 'Data Updated';
+    }
+    return ($results);
+}
 /**
  * TODO: Create model representing capstone project.
  */
--- app/views/contact.view.php
+++ app/views/contact.view.php
@@ -1,13 +1,51 @@
 <?php
 require('partials/head.php');
-require('partials/navbar.php');
 ?>
-
     <main>
+    <div class="flex flex-wrap place-items-center h-screen">
+    <section class="relative mx-auto">
+
+        <!-- navbar -->
+        <nav class="container flex justify-between text-gray-200 w-screen rounded-lg">
+            <div class="px-5 xl:px-12 py-3 flex w-full items-center rounded-lg bg-zinc-950/85 ring-1 ring-black/5 shadow-sm backdrop-blur-sm">
+
+                <!-- Logo -->
+                <a class="text-3xl font-bold font-heading hover:text-red-900" href="#">
+                    <i class="fa-solid fa-calendar-days mr-1 text-red-900"></i>
+                    <!-- <img class="h-9" src="logo.png" alt="logo"> -->
+                    do it.
+                </a>
+                <!-- Nav Links -->
+                <ul class=" md:flex px-4 mx-auto font-semibold font-heading space-x-12">
+                    <li><a class="hover:text-red-900" href="#">Services</a></li>
+                    <li><a class="hover:text-red-900" href="#">About</a></li>
+                    <li><a class="hover:text-red-900" href="#">Contact</a></li>
+                </ul>
+                <!-- Header Icons -->
+                <div class=" xl:flex items-center space-x-5 items-center">
+                    <!-- login button -->
+                    <button class="bg-red-900 hover:bg-red-950 text-white font-semibold py-2 px-4 rounded-full mr-1">
+                        Log in
+                    </button>
+                </div>
+            </div>
+        </nav>
+
+
+        <!-- site content -->
+        <div class="container mx-auto px-6 py-16 text-center">
+            <div class="mx-auto max-w-lg">
+                <h1 class="text-3xl font-bold text-gray-200 lg:text-4xl">Contact Page
+                </h1>
+            </div>
+
         <div>
-            <h2 class="text-center text-gray-300">More content of the site goes here. </h2>
             <br>
+            <h2 class="text-2xl text-center text-gray-300"><b>Support Email</b></h2>
+            <h3 class="text-center text-gray-400">doitsupport@gmail.com</h3>
             <br>
+            <h2 class="text-2xl text-center text-gray-300"><b>Phone Number</b></h2>
+            <h3 class="text-center text-gray-400">954-991-9521</h3>
             <br>
             <br>
             <br>
