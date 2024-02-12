1425983a543cdf7abfff41f523bc5adc06e6c762
TheJerryCann <122393082+TheJerryCann@users.noreply.github.com>
2024-02-11 22:39:39 -0500

    tasks and lists

---
+++ app/models/ListModel.php
@@ -0,0 +1,94 @@
+<?php
+
+// global $db originate from /db.php
+include(__DIR__ . '/../database/db.php');
+
+function getTasks () {
+    global $db;
+    $results = [];
+    $stmt = $db->prepare("SELECT task_id, list_id, title, description, due_date, is_favorite, created_at, updated_at From tasks ORDER BY due_date")
+
+    if ($stmt->execute() && $stmt->rowcount() > 0 ) {
+        $results = $stmt->fetchALL(PDO::FETCH_ASSOC);
+    }
+    return($results);
+}
+
+function addTask ($t, $d, $dd, $if){
+    global $db;
+    $stmt = $db->prepare("INSERT INTO tasks SET title = :title, description = :description, due_date = :due_date, is_favorite = :is_favorite");
+    $binds = array(
+        ":title" => $t,
+        ":description" => $d,
+        ":due_date" => $dd,
+        ":is_favorite" => $isf
+    );
+    if ($stmt->execute($binds) && $stmt->rowCount() > 0) {
+        $results = 'Data Added';
+    }
+    return ($results);
+}
+$tasks = getTasks();
+
+function deleteTask ($task_id){
+    global $db;
+    $results = [];
+    $sql = "DELETE FROM tasks WHERE task_id = ;task_id";
+    $stmt = $db->prepare("DELETE FROM tasks WHERE task_id = :task_id")
+    $binds = array(
+        ":task_id" => $task_id
+    );
+    if($stmt->execute($binds) && $stmt->rowCount() > 0) {
+        $results = 'Data Deleted';
+    }
+    return ($results);
+}
+
+function updateTask ($task_id, $title, $description, $due_date, $is_favorite, $created_at, $updated_at){
+    $results = [];
+    $sql = "UPDATE tasks SET title = :t, description = :d, due_date = :dd, is_favorite = :isf WHERE task_id = task_id";
+    $stmt = $db->prepare("UPDATE tasks SET title = :t, description = :d, due_date = :dd, is_favorite = :isf WHERE task_id = ;task_id");
+    $binds = array(
+        ":title" => $t,
+        ":description" => $d,
+        ":due_date" => $dd,
+        ":is_favorite" => $isf
+    );
+    if($stmt->execute($binds) && $stmt->rowCount() > 0) {
+        $results = 'Data Updated';
+    }
+    return ($results);
+}
+
+function searchTasks($title, $due_dater, $is_favorite)
+{
+    global $db;
+    $results = [];
+    $binds = array();
+    
+    $sql =  "SELECT * FROM  tasks WHERE 0=0";
+    if ($title != "") {
+        $sql .= " AND title LIKE :title";
+        $binds['title'] = '%'.$title.'%';
+    }
+
+    if ($due_date != "") {
+        $sql .= " AND due_date LIKE :due_date";
+        $binds['due_date'] = '%'.$due_date.'%';
+    }
+        
+    if ($is_favorite != "") {
+        $sql .= " AND is_favorite = :is_favorite";
+        $binds['is_favorite'] = $is_favorite;
+    }
+
+    $stmt = $db->prepare($sql);
+    if ($stmt->execute($binds) && $stmt->rowCount() > 0) {
+        $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
+    }
+    else{
+        $results = "No results";
+    }
+
+    return $results;
+}
