01e95178e9242ed69599f453507cbb276c2be6c4
TheJerryCann <122393082+TheJerryCann@users.noreply.github.com>
2024-02-23 13:54:19 -0500

    stuff

--- views/partials/viewTasks.php
+++ views/partials/viewTasks.php
@@ -1,21 +1,31 @@
 <!-- Page Content Area -->
 <div class="flex-1 p-4">
-    <div class="block text-gray-300 font-medium text-lg"><?= $listInfo['list_name'] . " List" ?></div>
+    <div class="block text-gray-300 font-semibold text-3xl"><?= $listInfo['list_name'] . " List" ?></div>
+    <div class="block text-gray-300 font-semibold text-md">List Info:</div>
     <div class="bg-gradient-to-r from-red-700 to-red-900 h-px mt-2"></div>
 
     <!-- Container for the 4 sections -->
-    <div class="grid grid-cols-1 md:grid-cols-2 gap-4 mt-2 p-2">
+    <div class="gap-4 mt-2 p-2 w-full mx-auto">
 
 
-        <div class="container mx-auto mt-6 px-3">
+        <div class="container mt-6 px-3">
             <?php if (is_array($tasks)&& count($tasks)> 0): ?>
-            <ul class="divide-y divide-stone-300">
+            <ul class="divide-y">
                 <?php foreach ($tasks as $task) : ?>
-                <a href="editTask.php?action=Update&taskID=<?= htmlspecialchars($task['task_id']) ?>">
-                    <li class="p-4 rounded-lg bg-zinc-950 my-4">
-                        <h3 class="text-lg font-semibold text-white"><?= htmlspecialchars($task['title']) ?></h3>
+
+                    <li class="p-4 rounded-lg bg-zinc-950 my-4 flex flex-row justify-between">
+                        <div class="flex flex-col">
+                            <a href="editTask.php?action=Update&taskID=<?= htmlspecialchars($task['task_id']) ?>"><h3 class="text-lg font-semibold text-white"'><?= htmlspecialchars($task['title']) ?></h3></a>
+                            <p class="text-white"><?= htmlspecialchars($task['description']) ?></p>
+                            <p class="text-white ml-3"><i class="fa-solid fa-calendar-day mr-2"></i> <?= htmlspecialchars($task['due_date']) ?></p>
+                            <input type="checkbox" class="text-white"> <?= htmlspecialchars($task['is_favorite']) ?></input>
+                        </div>
+
+                        <div class="flex items-center ml-10">
+                            <i class="fa-regular fa-trash-can text-red-800"></i>
+                        </div>
                     </li>
-                </a>
+
                 <?php endforeach; ?>
             </ul>
             <?php else: ?>
