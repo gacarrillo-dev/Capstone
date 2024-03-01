b547627ebb08e007cb50e56fdbf6ebd3752a8e96
Gabriel <117479504+gacarrillo-dev@users.noreply.github.com>
2024-03-01 12:26:11 -0500

    fixed merging issue with main

--- views/partials/sidebar.php
+++ views/partials/sidebar.php
@@ -1,7 +1,7 @@
 <!-- Sidebar container -->
 <div class="p-2 bg-zinc-950/85 w-60 flex flex-col hidden md:flex" id="sideNav">
     <nav>
-        <a class="block text-gray-300 py-2.5 px-4 my-4 rounded transition duration-200 hover:bg-gradient-to-r hover:from-red-900 hover:to-red-800 hover:text-white" href="#">
+        <a class="block text-gray-300 py-2.5 px-4 my-4 rounded transition duration-200 hover:bg-gradient-to-r hover:from-red-900 hover:to-red-800 hover:text-white" href="userHomePage.php">
             <i class="fas fa-home mr-3 text-gray-500"></i>Home
         </a>
         <a class="block text-gray-300 py-2.5 px-4 my-4 rounded transition duration-200 hover:bg-gradient-to-r hover:from-red-900 hover:to-red-800 hover:text-white" href="#">
@@ -10,8 +10,8 @@
         <a class="block text-gray-200 py-2.5 px-4 my-4 rounded transition duration-200 hover:bg-gradient-to-r hover:from-red-900 hover:to-red-800 hover:text-white" href="#">
             <i class="fa-regular fa-circle-right mr-3 text-gray-500"></i>Up Coming
         </a>
-        <a class="block text-gray-200 py-2.5 px-4 my-4 rounded transition duration-200 hover:bg-gradient-to-r hover:from-red-900 hover:to-red-800 hover:text-white" href="#">
-            <i class="fas fa-users mr-3 text-gray-500"></i>Shared
+        <a class="block text-gray-200 py-2.5 px-4 my-4 rounded transition duration-200 hover:bg-gradient-to-r hover:from-red-900 hover:to-red-800 hover:text-white" href="favorites.php">
+            <i class="fa-solid fa-star mr-3 text-gray-500"></i>Favorites
         </a>
 
         <!-- separation border -->
@@ -31,7 +31,7 @@
             // Get the task count for the current list
             $task_count = get_task_count_for_list($user_list['list_id']);
             ?>
-            <a class="block text-gray-200 py-2.5 px-4 my-4 rounded transition duration-200 hover:bg-zinc-900/85 hover:text-white flex justify-between items-center" href="/lists?id<?= htmlspecialchars($user_list['list_id']) ?>">
+            <a class="block text-gray-200 py-2.5 px-4 my-4 rounded transition duration-200 hover:bg-zinc-900/85 hover:text-white flex justify-between items-center" href="list.php?listID=<?= htmlspecialchars($user_list['list_id']) ?>">
                 <span><i class="fa-solid fa-list-ul mr-2 text-gray-500 text-sm"></i><?= htmlspecialchars($user_list['list_name']) ?></span>
                 <?php if ($task_count > 0) : ?>
                     <span class="text-red-800 font-semibold ml-auto"><?= $task_count ?></span>
